a4a45420be885942797c42d4e2692c3e1e4a0ec8
Tomm Smith <root.packet@gmail.com>
2025-01-04 14:37:52 -0500

    Finished crude UI, Morris Class created, GUI Morris incorporation

--- main.py
+++ main.py
@@ -6,15 +6,135 @@ import tkinter as tk
 
 
 
+
 class Morris:
+    text = {
+            '.-'    : 'A',
+            '-...'  : 'B',
+            '-.-.'  : 'C',
+            '-..'   : 'D',
+            '.'     : 'E',
+            '..-.'  : 'F',
+            '--.'   : 'G',
+            '....'  : 'H',
+            '..'    : 'I',
+            '.---'  : 'J',
+            '-.-'   : 'K',
+            '.-..'  : 'L',
+            '--'    : 'M',
+            '-.'    : 'N',
+            '---'   : 'O',
+            '.--.'  : 'P',
+            '--.-'  : 'Q',
+            '.-.'   : 'R',
+            '...'   : 'S',
+            '-'     : 'T',
+            '..-'   : 'U',
+            '...-'  : 'V',
+            '.--'   : 'W',
+            '-..-'  : 'X',
+            '-.--'  : 'Y',
+            '--..'  : 'Z',
+            '.----' : '0',
+            '..---' : '1',
+            '...--' : '2',
+            '....-' : '3',
+            '.....' : '4',
+            '-....' : '5',
+            '--...' : '6',
+            '---..' : '7',
+            '----.' : '8',
+            '-----' : '9'
+            }
+    morse = {
+            'A' : '.-',
+            'B' : '-...',
+            'C' : '-.-.',
+            'D' : '-..',
+            'E' : '.',
+            'F' : '..-.',
+            'G' : '--.',
+            'H' : '....',
+            'I' : '..',
+            'J' : '.---',
+            'K' : '-.-',
+            'L' : '.-..',
+            'M' : '--',
+            'N' : '-.',
+            'O' : '---',
+            'P' : '.--.',
+            'Q' : '--.-',
+            'R' : '.-.',
+            'S' : '...',
+            'T' : '-',
+            'U' : '..-',
+            'V' : '...-',
+            'W' : '.--',
+            'X' : '-..-',
+            'Y' : '-.--',
+            'Z' : '--..',
+            '0' : '.----',
+            '1' : '..---',
+            '2' : '...--',
+            '3' : '....-',
+            '4' : '.....',
+            '5' : '-....',
+            '6' : '--...',
+            '7' : '---..',
+            '8' : '----.',
+            '9' : '-----'
+            }
+
+    def __init__(self):
+            None
+            
+    def t2m(self, code):
+        """ t2m() - Text 2 Morse """
+        m_code = ""
+        
+        for char in code:
+            # Check for valid character in dict, pass and print otherwise
+            try:
+                self.morse[char.upper()]
+            except KeyError:
+                print("DEBUG: t2m() - except KeyError: Invalid Key in dict")
+                continue
+            # Append code 
+            else:
+                m_code = m_code + self.morse[char.upper()] + ' '
+            
+        return m_code
+        
+    def m2t(self, code):
+        """ m2t() - Morse 2 Text"""
+        t_code = ""
+        m_word = ""
+        
+        for char in code:
+            # Morse word terminated by white space, handle word
+            if char == ' ':
+                try:
+                    self.text[m_word]
+                except KeyError:
+                    print("DEBUG: m2t() - except KeyError: Invalid Key in dict" + m_word)
+                else:
+                    t_code = t_code + self.text[m_word] + ' '
+                    m_word = ''
+                    
+            # Store morris character 
+            else:
+                m_word = m_word + char
+
+        return t_code
+
+
+class GUI:
     def __init__(self):
         self.root = tk.Tk()
-        #self.root.geometry("777x575")
+        self.root.geometry("777x575")
         #self.root.minsize(777, 575)
-        #self.root.columnconfigure(0, weight=1)
-        #self.root.columnconfigure(1, weight=200)
-        #self.root.rowconfigure(0, weight=1)
         
+        self.morris = Morris()
         
         # Text Code
         self.text_code_lbl = tk.Label(self.root, text="Text Code:")
@@ -30,13 +150,17 @@ class Morris:
         self.morse_code = tk.Text(self.root, height=1, width=1)
         self.morse_code.pack(fill="both", expand=True)
         
-        # Navigation
+        
+        # UI Buttons
         self.nav_btn_frame = tk.Frame(self.root)
         self.nav_btn_frame.pack(side="right", expand="False", fill="none")
         
-        # Generate
-        self.morse_btn = tk.Button(self.nav_btn_frame, text="Generate", command=self.__morse_btn_clicked__)
+        # Morse
+        self.morse_btn = tk.Button(self.nav_btn_frame, text="Morse", command=self.__morse_btn_clicked__)
         self.morse_btn.pack(side="right", expand="False", fill="none")
+        # Text
+        self.text_btn = tk.Button(self.nav_btn_frame, text="Text", command=self.__text_btn_clicked__)
+        self.text_btn.pack(side="right", expand="False", fill="none")
         # Clear
         self.clear_btn = tk.Button(self.nav_btn_frame, text="Clear", command=self.__clear_btn_clicked__)
         self.clear_btn.pack(side="right", expand="False", fill="none")
@@ -55,24 +179,36 @@ class Morris:
         root.config(menu=self.menu)
         
     def __clear_btn_clicked__(self):
-        None
+        self.text_code.delete("0.0", "end")
+        self.morse_code.delete("0.0", "end")
         
+    def __text_btn_clicked__(self):
+        morse = self.morse_code.get("0.0", "end")
+        
+        if morse != '\n':
+            self.text_code.delete("0.0", "end")
+            self.text_code.insert("0.0", self.morris.m2t(morse))
+            
     def __morse_btn_clicked__(self):
-        None
+        text = self.text_code.get("0.0", "end")
         
-    def __text_code__(self):
-        None
-        
-    def __morse_code__(self):
-        None
-        
-    def __buttons__(self):
-        None
+        if text != '\n':
+            self.morse_code.delete("0.0", "end")
+            self.morse_code.insert("0.0", self.morris.t2m(text))
         
 
 
 
 
 if __name__ == "__main__":
-    morris = Morris()
-    tk.mainloop() 
+    morris = GUI()
+    tk.mainloop()
+    
+    m = Morris()
+    
+    morse = m.t2m("hello")
+    text = m.m2t(morse)
+    
+    print("Text: ###" + text +"###")
+    print("Morse: ###" + morse + "###")
+    
